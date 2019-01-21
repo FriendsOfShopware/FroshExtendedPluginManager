f5bd74d8eb18af76ceba870fe7fcdda0123e3b9a
tinect <s.koenig@tinect.de>
2019-01-21 19:50:28 +0100

    Revert "extend SubscriptWarning"
    This reverts commit 2ac36c40d829b1f2267fc66ee87fe62eb1cac715.

--- FroshExtendedPluginManager.php
+++ FroshExtendedPluginManager.php
@@ -11,23 +11,8 @@ class FroshExtendedPluginManager extends Plugin
     {
 
         return [
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_PluginManager' => 'onPluginManagerPostDispatch',
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_Base' => 'onBackendPostDispatch'
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_PluginManager' => 'onPluginManagerPostDispatch'
         ];
-    }
-
-    public function onBackendPostDispatch(\Enlight_Event_EventArgs $args)
-    {
-        /** @var \Enlight_Controller_Action $controller */
-        $controller = $args->getSubject();
-        $view = $controller->View();
-        $request = $controller->Request();
-
-        $view->addTemplateDir(__DIR__ . '/Views');
-
-        if ($request->getActionName() == 'index') {
-            $view->extendsTemplate('backend/base/component/extendPM.notification.SubscriptionWarning.js');
-        }
     }
 
     public function onPluginManagerPostDispatch(\Enlight_Event_EventArgs $args)
@@ -42,6 +27,7 @@ class FroshExtendedPluginManager extends Plugin
 
         if ($request->getActionName() == 'load') {
             $view->extendsTemplate('backend/plugin_manager/view/extendPM/detail/Container.js');
+
             $view->extendsTemplate('backend/plugin_manager/view/extendPM/list/local_plugin_listing_page.js');
         }
     }
